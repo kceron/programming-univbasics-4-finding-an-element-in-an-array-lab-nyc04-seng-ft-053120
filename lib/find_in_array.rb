5ffcb91f01150729bf6034cde9c1f3cda719214a
Karem Ceron <karemiceron@gmail.com>
2020-06-03 03:00:55 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,13 @@
+=begin 
 def find_element_index(array, value_to_find)
   count = 0 
   while count < array.size do 
     return array.index(value_to_find)
     count += 1 
   end 
+end
+=end
+
+def find_element_index(array, value_to_find)
+  array.index(value_to_find)
 end
